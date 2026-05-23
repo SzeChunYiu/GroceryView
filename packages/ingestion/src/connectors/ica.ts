8771eefdf334c387fc88b20a7022135fd7adfb8f
Sze Chun YIU <52784502+SzeChunYiu@users.noreply.github.com>
2026-05-23 14:21:43 +0200

    feat(ingest): add ICA Kvantum Kista promotions (#1308)
    Co-authored-by: grocery-laptop <grocery-laptop@babbloo.local>

--- packages/ingestion/src/connectors/ica.ts
+++ packages/ingestion/src/connectors/ica.ts
@@ -1636,6 +1636,11 @@ export const DEFAULT_ICA_STORE_CONFIGS: readonly IcaStoreConfig[] = [
     regionId: DEFAULT_ICA_REGION_ID
   },
   {
+    storeAccountId: '1004587',
+    storeName: 'ICA Kvantum Kista',
+    regionId: DEFAULT_ICA_REGION_ID
+  },
+  {
     storeAccountId: '1003801',
     storeName: 'ICA Kvantum Tidaholm',
     regionId: DEFAULT_ICA_REGION_ID
