24ce6c32d1244a9ae5ea3f389bc3d4bf2cb86499
billy <billy10384@gmail.com>
2026-05-25 19:37:17 +0200

    Add product brands table

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -14,6 +14,7 @@ export * from './queries/stores.js';
 export * from './queries/retailers.js';
 export * from './queries/users.js';
 export * from './seed/retailers.js';
+export * from './seed/brands.js';
 
 export type Migration = {
   version: string;
