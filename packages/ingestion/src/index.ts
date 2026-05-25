ad137d7ae0894bb56e2ae1860f7736fec426b180
billy <billy10384@gmail.com>
2026-05-25 16:55:19 +0200

    Add Iceland POI audit ingestion job

--- packages/ingestion/src/index.ts
+++ packages/ingestion/src/index.ts
@@ -118,6 +118,7 @@ export * from './connectors/openfoodfacts.js';
 export * from './connectors/all-store-runner.js';
 export * from './connectors/overpass.js';
 export * from './connectors/fuel-stations.js';
+export * from './jobs/is-poi-audit.js';
 export * from './connectors/citygross.js';
 export * from './connectors/citygross-bulk.js';
 export * from './connectors/coop.js';
