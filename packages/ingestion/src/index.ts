929aeaad8bf4b54394f9c4e11e05b8e828ee317d
billy <billy10384@gmail.com>
2026-05-25 19:29:52 +0200

    Add Reklamblad SE flyer connector

--- packages/ingestion/src/index.ts
+++ packages/ingestion/src/index.ts
@@ -151,6 +151,7 @@ export * from './connectors/matpriskollen.js';
 export * from './connectors/matspar.js';
 export * from './connectors/meny-no.js';
 export * from './connectors/normal-se.js';
+export * from './connectors/reklamblad-se.js';
 export {
   LIDL_PLUS_COUPONS_URL,
   fetchLidlPlusCoupons,
