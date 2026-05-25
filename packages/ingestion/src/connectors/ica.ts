5f3308f9354cfeb8bd271d6fd0ebdea4af5b84ed
pr-merger-p2 <pr-merger-p2@users.noreply.github.com>
2026-05-25 05:05:46 +0200

    feat(ingest): add ICA Vagnharad connector

--- packages/ingestion/src/connectors/ica.ts
+++ packages/ingestion/src/connectors/ica.ts
@@ -1659,6 +1659,11 @@ export const DEFAULT_ICA_STORE_CONFIGS: readonly IcaStoreConfig[] = [
     regionId: DEFAULT_ICA_REGION_ID
   },
   {
+    storeAccountId: '1004281',
+    storeName: 'ICA Supermarket Vagnhärad',
+    regionId: DEFAULT_ICA_REGION_ID
+  },
+  {
     storeAccountId: '1004587',
     storeName: 'ICA Kvantum Kista',
     regionId: DEFAULT_ICA_REGION_ID
